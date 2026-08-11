722bc3fe5f64441258b5dd3bcde319965fddc979
Oddlyastute <53821530+Oddlyastute@users.noreply.github.com>
2026-08-11 00:27:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,26 @@
 // Light / Dark Mode Toggle
 
-const themeToggle = document.getElementById("theme-toggle");
-const themeIcon = document.getElementById("theme-icon");
+let darkmode = localStorage.getItem("darkmode");
+const themeSwitch = document.getElementById("theme-switch");
 
-if (themeToggle && themeIcon) {
+const enableDarkmode = () => {
+    document.body.classList.add("darkmode");
+    localStorage.setItem("darkmode", "active");
+};
 
-    themeToggle.addEventListener("click", function () {
+const disableDarkmode = () => {
+    document.body.classList.remove("darkmode");
+    localStorage.setItem("darkmode", null);
+};
 
-        document.body.classList.toggle("dark-mode");
-
-        if (document.body.classList.contains("dark-mode")) {
-
-            themeIcon.src = "dark_mode.svg";
-
-            themeToggle.setAttribute(
-                "aria-label",
-                "Switch to light mode"
-            );
-
-        } else {
-
-            themeIcon.src = "light_mode.png";
-
-            themeToggle.setAttribute(
-                "aria-label",
-                "Switch to dark mode"
-            );
-
-        }
-
-    });
-
+if (darkmode === "active") {
+    enableDarkmode();
 }
+
+themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem("darkmode");
+
+    darkmode !== "active"
+        ? enableDarkmode()
+        : disableDarkmode();
+});
